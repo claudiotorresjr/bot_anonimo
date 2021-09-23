c672225a0c12055f541907411585b97a5c34a3aa
ctj17 <ctj17@inf.ufpr.br>
2021-09-23 20:35:55 -0300

    inicio

--- anomi.py
+++ anomi.py
@@ -23,25 +23,25 @@ class AnonimousBot(commands.Cog):
             channel_id = 259108205798359040
 
             message_content = message.content
-            msg = message_content.split("@")
-            if len(msg) >= 1:
-                if len(msg) > 1:
-                    name = msg[1].split("#")[0]
-                    code = msg[1].split("#")[1].split(" ")[0]
-                else:
-                    name = msg[0].split("#")[0]
-                    code = msg[0].split("#")[1].split(" ")[0]
+            if "@" in message_content:
+                msg = message_content.split("@")
+                if len(msg) >= 1:
+                    if len(msg) > 1:
+                        name = msg[1].split("#")[0]
+                        code = msg[1].split("#")[1].split(" ")[0]
+                    else:
+                        name = msg[0].split("#")[0]
+                        code = msg[0].split("#")[1].split(" ")[0]
 
-                guild = self.bot.get_guild(channel_id)
-                
-                mention = f"{name}#{code}"
-                user_id_to_mention = ""
-                for m in guild.members:
-                    if str(m) == mention:
-                        print(name, code)
-                        user_id_to_mention = f"<@{m.id}>"
-                        break
-                
-                message_content = message_content.replace(f"@{mention}", user_id_to_mention)
+                    guild = self.bot.get_guild(channel_id)
+                    
+                    mention = f"{name}#{code}"
+                    user_id_to_mention = ""
+                    for m in guild.members:
+                        if str(m) == mention:
+                            user_id_to_mention = f"<@{m.id}>"
+                            break
+                    
+                    message_content = message_content.replace(f"@{mention}", user_id_to_mention)
 
             await self.bot.get_channel(channel_id).send(message_content)
